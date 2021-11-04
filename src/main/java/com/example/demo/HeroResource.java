a29f62544fe1b6d74eac4828aa6e14b4d2306663
Vinicius Barbosa Ponte <50773659+ViniciusPonte@users.noreply.github.com>
2021-11-03 21:12:48 -0300

    Update HeroResource.java

--- src/main/java/com/example/demo/HeroResource.java
+++ src/main/java/com/example/demo/HeroResource.java
@@ -15,22 +15,26 @@ public class HeroResource {
         this.repository = repository;
     }
 
+    @CrossOrigin()
     @GetMapping("/all")
     public List<Hero> users(){
         return repository.findAll();
     }
 
+    @CrossOrigin()
     @PostMapping("save")
     public Hero save(@RequestBody Hero hero){
         Hero createdNewFavoriteHero = repository.save(hero);
         return createdNewFavoriteHero;
     }
 
+    @CrossOrigin()
     @GetMapping("{id_user}")
     public List<Hero> getHeroes(@PathVariable int id_user){
         return repository.findByParentId(id_user);
     }
 
+    @CrossOrigin()
     @DeleteMapping("{id}")
     public String delete(@PathVariable int id){
         repository.deleteById(id);
