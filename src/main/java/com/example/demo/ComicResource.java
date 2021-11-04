13ff0e94545819bede064d76ba9c2ba07d87a44c
Vinicius Barbosa Ponte <50773659+ViniciusPonte@users.noreply.github.com>
2021-11-03 21:12:16 -0300

    Update ComicResource.java

--- src/main/java/com/example/demo/ComicResource.java
+++ src/main/java/com/example/demo/ComicResource.java
@@ -15,22 +15,26 @@ public class ComicResource {
         this.repository = repository;
     }
 
+    @CrossOrigin()
     @GetMapping("/all")
     public List<Comic> users(){
         return repository.findAll();
     }
 
+    @CrossOrigin()
     @PostMapping("save")
     public Comic save(@RequestBody Comic comic){
         Comic createdNewFavoriteComic = repository.save(comic);
         return createdNewFavoriteComic;
     }
 
+    @CrossOrigin()
     @GetMapping("{id_user}")
     public List<Comic> getHeroes(@PathVariable int id_user){
         return repository.findByParentId(id_user);
     }
 
+    @CrossOrigin()
     @DeleteMapping("{id}")
     public String delete(@PathVariable int id){
         repository.deleteById(id);
